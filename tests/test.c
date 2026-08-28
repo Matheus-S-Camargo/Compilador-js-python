e0edff06b0e52f9739f917e4de252890709ee0ce
Kodomoppoi <jcmm.esft@gmail.com>
2026-08-28 20:51:30 -0300

    leve modificação do test.c no yywrap, adicao e teste de um novo codigo js para o teste do lexema

--- tests/test.c
+++ tests/test.c
@@ -1,15 +1,12 @@
 #include "grammar.tab.h"
 #include <stdio.h>
 
-
 extern int yylex(void);
 extern char *yytext;
 
-int yywrap(void) {
-  return 1; // 1 indica que o arquivo terminou e a leitura deve parar
-}
+int yywrap() { return 1; }
 
-int main(void) {
+int main() {
   int token;
   while ((token = yylex()) != 0) {
     printf("TOKEN: %-15d LEXEMA: '%s'\n", token, yytext);
